d8882802c6f47626800bdacc49956c73d33c8b5e
unknown <Shvab_A@IT-01.iuca.loc>
2014-12-11 13:34:29 +0600

    пробная

---
+++ arr2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?
+for ($i=0; $i<=100; $i++){
+	$arr[$i]=rand (1,100);
+}
+foreach ($arr as $val){
+	$arr2;
+}
+for ($i=0; $i<=100; $i++){
+	if ($arr2<=0){
+		echo "$arr2 minimalnoe chislo";
+	}
+}
+?>
+</body>
+</html>
